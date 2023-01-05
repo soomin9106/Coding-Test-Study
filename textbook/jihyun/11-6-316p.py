ddf10e074ed5c87c891f402c05e3658de5d3908b
이지현 <iblue@MacBook-Air.local>
2023-01-05 21:40:40 +0900

    sol 11-6-316p.py

--- textbook/jihyun/11-6-316p.py
+++ textbook/jihyun/11-6-316p.py
@@ -2,23 +2,39 @@
 import heapq
 
 def solution(food_times, k):
+    if sum(food_times) <= k:
+        return -1
+
     heap = []
 
     for i in range(len(food_times)):
         heapq.heappush(heap, (food_times[i], i))
 
     prev = 0
-    while k > len(heap):
-        root = heapq.heappop(heap)[1]
+    while True:
+        length = len(heap)
+        root = heap[0][0]
 
         height = root - prev
-        width = len(heap)
+        width = length
+        area = height * width
+
+        if k < area:
+            break
 
         k -= height * width
 
         prev = root
+        heapq.heappop(heap)
+
+    heap.sort(key=lambda x:x[1])
+    print(heap)
+
+    x = k % len(heap)
+    print(heap[x][1] + 1)
+
+    return heap[x][1] + 1
     
-    print(k)
 
 
 def main():

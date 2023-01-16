4db6704b6459f29c966fb2e5cdce3fd6f09d2eb1
이지현 <iblue@MacBook-Air.local>
2023-01-16 15:24:56 +0900

    example 8-1-ex2-212p add fibo_bottom_up function

--- textbook/jihyun/example/8-1-ex2-212p.py
+++ textbook/jihyun/example/8-1-ex2-212p.py
@@ -2,7 +2,6 @@
 from time import time
 
 MAX_LEN = 99
-d = [0] * (MAX_LEN + 1)
 
 def fibonacci(x):
     if x == 1 or x == 2:
@@ -10,7 +9,7 @@ def fibonacci(x):
 
     return fibonacci(x - 1) + fibonacci(x - 2)
 
-def fibo_memoization(x):
+def fibo_memoization(d, x):
 
     if x == 1 or x == 2:
         return 1
@@ -18,7 +17,15 @@ def fibo_memoization(x):
     if d[x] != 0:
         return d[x]
 
-    d[x] = fibo_memoization(x - 1) + fibo_memoization(x - 2)
+    d[x] = fibo_memoization(d, x - 1) + fibo_memoization(d, x - 2)
+    
+    return d[x]
+
+def fibo_bottom_up(d, x):
+    d[1], d[2] = 1, 1
+
+    for i in range(3, x + 1):
+        d[i] = d[i - 1] + d[i - 2]
     
     return d[x]
 
@@ -27,8 +34,14 @@ def main():
     print(fibonacci(30))
     print(f"time: {time() - st}")
 
+    d = [0] * (MAX_LEN + 1)
     st = time()
-    print(fibo_memoization(30))
+    print(fibo_memoization(d, 30))
+    print(f"time: {time() - st}")
+
+    d = [0] * (MAX_LEN + 1)
+    st = time()
+    print(fibo_bottom_up(d, 30))
     print(f"time: {time() - st}")
 
 if __name__ == "__main__":

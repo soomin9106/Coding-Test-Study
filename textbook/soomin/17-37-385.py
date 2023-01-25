e81415f76baef7fab496d5e141ebede7c1a79f92
soomin9106 <soomin9106@naver.com>
2023-01-25 21:25:28 +0900

    플로이드 문제 풀이

---
+++ textbook/soomin/17-37-385.py
@@ -0,0 +1,36 @@
+# 플로이드
+# https://www.acmicpc.net/problem/11404
+
+import sys
+
+sys.stdin = open("textbook/soomin/input.txt","rt")
+
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if i == j:
+            graph[i][j] = 0
+
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = min(graph[a][b], c)
+
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if graph[a][b] == INF:
+            print(0, end=' ')
+        else:
+            print(graph[a][b], end=" ")
+    print()
